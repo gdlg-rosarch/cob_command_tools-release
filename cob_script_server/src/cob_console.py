82d0abbba75c750b24f4413e0940a235c135dc0d
flg-pb <philip.bell@ipa.fraunhofer.de>
2017-07-17 17:02:33 +0200

    Revert "Trimmed the branch to only the cob_monitoring sub directory"
    This reverts commit b0e424ecdff38e367a3fa59356f453a66b80fbb5.

---
+++ cob_script_server/src/cob_console.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import rospy
+from simple_script_server import *
+sss = simple_script_server()
+
+### start this with
+# rosrun cob_script_server cob_console
+
+if __name__ == "__main__":
+	rospy.init_node("cob_console")
